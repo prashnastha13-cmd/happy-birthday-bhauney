cef796bb48b6a60ace2c093cd49e0083a145be2a
prashnastha13-cmd <prashnastha13@gmail.com>
2026-07-04 18:25:31 +0545

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ launchFireworks();
 },2500);
 
 }
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.className="heart";
+
+    heart.innerHTML="❤️";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.fontSize=(20+Math.random()*20)+"px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },6000);
+
+}
